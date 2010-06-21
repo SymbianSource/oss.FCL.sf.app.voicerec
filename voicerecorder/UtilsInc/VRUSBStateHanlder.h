6ae8e9d748fc77ae92acbb069182bbfabfa8d1d3
Dremov Kirill (Nokia-D-MSW/Tampere) <kirill.dremov@nokia.com>
2010-06-21 15:43:26 +0300

    Revision: 201022 Kit: 2010125
    --HG--
    branch : RCL_3

--- voicerecorder/UtilsInc/VRUSBStateHanlder.h
+++ voicerecorder/UtilsInc/VRUSBStateHanlder.h
@@ -1,12 +1,22 @@
 /*
- ============================================================================
- Name		: VRUSBStateHanlder.h
- Author	  : Junhua Xu
- Version	 : 1.0
- Copyright   : Your copyright notice
- Description : CVRUSBStateHanlder declaration
- ============================================================================
- */
+* Copyright (c) 2002-2006 Nokia Corporation and/or its subsidiary(-ies).
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+*
+* Initial Contributors:
+* Nokia Corporation - initial contribution.
+*
+* Contributors:
+*
+* Description: 
+*     CVRUSBStateHanlder declaration
+*     
+*
+*/
+
 
 #ifndef VRUSBSTATEHANLDER_H
 #define VRUSBSTATEHANLDER_H
